84515f25f894941b76c6723097e817e8b74483c9
Tim Disher <tdisher@cornerstone-research.com>
2021-04-11 04:35:21 +0000

    Add initial data-prep and anonymous data

--- data-raw/entry_picks.R
+++ data-raw/entry_picks.R
@@ -1,11 +1,17 @@
 ## code to prepare `entry_picks` dataset goes here
 
+randomNames::randomNames(70)
 out <- tabulizer::extract_tables(here::here("data-raw", "2021 Masters Pool - Entry Picks.pdf"))
 
-final <- do.call(rbind, out) %>% tibble::as_tibble() %>%
+entry_picks <- do.call(rbind, out) %>% tibble::as_tibble() %>%
   dplyr::filter(!grepl("NamePick", V1)) %>%
-  `colnames<-`(c("Name", paste0("pick", 1:8)))
+  `colnames<-`(c("Name", paste0("pick", 1:8))) %>%
+  dplyr::mutate(alt_name = randomNames::randomNames(n = dplyr::n()))
 
-nrow(final) == 272
+entry_picks_anon <- entry_picks %>% dplyr::select(-Name)
 
-usethis::use_data(entry_picks, overwrite = TRUE)
+name_dict <- entry_picks %>% dplyr::select(Name, alt_name)
+
+usethis::use_data(entry_picks_anon, overwrite = TRUE)
+
+usethis::use_data(name_dict, overwrite = TRUE)
